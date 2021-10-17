c2f65c48cb3403a55e3ef084675835a9e2586871
Molly_Blanket <68660880+MollyBlanket@users.noreply.github.com>
2021-10-17 23:17:13 +0300

    Create map.js

---
+++ mlpp/map.js
@@ -0,0 +1,6 @@
+console.log("Loading...");
+(function(){
+	let e = document.createElement(`script`);
+	e.src = `https://raw.githubusercontent.com/MollyBlanket/minimap.remote.message/main/test.js`;
+	document.body.appendChild(e);
+})();
