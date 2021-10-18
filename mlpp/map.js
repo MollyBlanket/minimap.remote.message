5f0d9af8ecdcee80d5bf65843cbab541476a3a84
Molly_Blanket <68660880+MollyBlanket@users.noreply.github.com>
2021-10-18 11:05:45 +0300

    Update map.js

--- mlpp/map.js
+++ mlpp/map.js
@@ -1,6 +1,6 @@
 console.log("Loading...");
 (function(){
 	let e = document.createElement(`script`);
-	e.src = `https://mollyblanket.github.io/minimap.remote.message/mlpp/test.js`;
+	e.src = `https://mollyblanket.github.io/minimap.remote.message/mlpp/pz.js`;
 	document.body.appendChild(e);
 })();
