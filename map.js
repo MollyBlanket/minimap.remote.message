e7978bb30a0c8a13142c604a5d9c45bb2f1ebeee
Molly_Blanket <68660880+MollyBlanket@users.noreply.github.com>
2021-10-18 10:48:56 +0300

    Delete map.js

--- map.js
+++
@@ -1,6 +0,0 @@
-console.log("Loading...");
-(function(){
-	let e = document.createElement(`script`);
-	e.src = `https://raw.githubusercontent.com/MollyBlanket/minimap.remote.message/main/test.js`;
-	document.body.appendChild(e);
-})();
